756c70f41d3ba7dd3037fc3cf6e01e2cc78984dc
garazh228 <adylbeksadykbekov.9.02@gmail.com>
2024-01-29 07:10:52 +0600

    home work

---
+++ 4.5 lesson/Struct.swift
@@ -0,0 +1,29 @@
+//
+//  Struct.swift
+//  4.5 lesson
+//
+//  Created by adyl CEO on 29/01/2024.
+//
+
+import UIKit
+
+class Struct: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
